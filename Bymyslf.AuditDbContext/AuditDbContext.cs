bbca19c1787a00899aa1da51e19ce87bdd4c89ac
Luis Barbosa <luis.barbosa@farfetch.com>
2016-10-05 11:45:16 +0100

    Initial Commit

---
+++ Bymyslf.AuditDbContext/AuditDbContext.cs
@@ -0,0 +1,72 @@
+﻿namespace Bymyslf.AuditDbContext
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Core;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class AuditDbContext : DbContext
+    {
+        public DbSet<AuditLog> AuditLogs { get; set; }
+
+        protected virtual void Audit()
+        {
+            var modifiedEntities = ChangeTracker.Entries()
+                                                .Where(p => p.State == EntityState.Deleted || p.State == EntityState.Modified)
+                                                .ToList();
+
+            var dateChanged = DateTime.UtcNow;
+
+            foreach (var entity in modifiedEntities)
+            {
+                var entityName = entity.Entity.GetType().Name;
+                var primaryKeyFieldAndValue = this.GetPrimaryKeyFieldAndValue(entity);
+
+                foreach (var propertyName in entity.OriginalValues.PropertyNames)
+                {
+                    var prop = entity.Property(propertyName);
+                    if (prop.IsModified)
+                    {
+                        var originalValue = (entity.OriginalValues[propertyName] ?? string.Empty).ToString();
+                        var currentValue = (entity.CurrentValues[propertyName] ?? string.Empty).ToString();
+                        if (originalValue != currentValue)
+                        {
+                            this.AuditLogs.Add(new AuditLog()
+                            {
+                                EntityName = entityName,
+                                PrimaryKeyField = primaryKeyFieldAndValue.Item1.ToString(),
+                                PrimaryKeyValue = primaryKeyFieldAndValue.Item2.ToString(),
+                                PropertyName = propertyName,
+                                Action = entity.State.ToString("G"),
+                                OldValue = originalValue,
+                                NewValue = currentValue,
+                                DateChanged = dateChanged
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
+        public override int SaveChanges()
+        {
+            this.Audit();
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync()
+        {
+            this.Audit();
+            return base.SaveChangesAsync();
+        }
+
+        //http://stackoverflow.com/questions/7255089/get-the-primary-key-value-of-an-arbitrary-entity-in-code-first/8083687#8083687
+        private Tuple<EntityKey, object> GetPrimaryKeyFieldAndValue(DbEntityEntry entry)
+        {
+            var objectStateEntry = ((IObjectContextAdapter)this).ObjectContext.ObjectStateManager.GetObjectStateEntry(entry.Entity);
+            return Tuple.Create(objectStateEntry.EntityKey, objectStateEntry.EntityKey.EntityKeyValues[0].Value);
+        }
+    }
+}
---
+++ Bymyslf.AuditDbContext/AuditLog.cs
@@ -0,0 +1,25 @@
+﻿namespace Bymyslf.AuditDbContext
+{
+    using System;
+
+    public class AuditLog
+    {
+        public int Id { get; set; }
+
+        public string EntityName { get; set; }
+
+        public string PrimaryKeyField { get; set; }
+
+        public string PrimaryKeyValue { get; set; }
+
+        public string PropertyName { get; set; }
+
+        public string Action { get; set; }
+
+        public string OldValue { get; set; }
+
+        public string NewValue { get; set; }
+
+        public DateTime DateChanged { get; set; }
+    }
+}
